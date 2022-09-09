4155823e0e916235b1ddb74fd4b7a66912dbed4a
Audun <audunyoung@gmail.com>
2022-09-09 14:43:57 -0600

    Set up initial header structure.

---
+++ src/components/PageHeader/PageHeader.js
@@ -0,0 +1,18 @@
+import './PageHeader.scss';
+//import { Link } from 'react-router-dom';
+
+const PageHeader = () => {
+    return (
+        <div className='header'>
+            <h1 className='header__logo'>AUDUN LOLA</h1>
+            <div className='header__nav'>
+                <h3 className='header__nav-page'>Projects</h3>
+                <h3 className='header__nav-page'>About</h3>
+                <h3 className='header__nav-page'>Contact</h3>
+                <h3 className='header__nav-page'>Fun</h3>
+            </div>
+        </div>
+    )
+}
+
+export default PageHeader;
