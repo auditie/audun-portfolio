d4bd1171321c49ae882c8a0dd60b16713047d8b3
Audun <audunyoung@gmail.com>
2022-10-10 16:43:37 -0600

    Start changing structure and styling to new design.

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,17 +4,7 @@ import './ProjectCard.scss';
 const ProjectCard = () => {
     return (
         <div className='project'>
-            <div className='project-overview'>
-                <h4 className='project__title'>Project Title</h4>
-                <div className='project__image'></div>
-            </div>
-            <div className='project-about'>
-                <p className='project__descrip'>I'm baby poke kinfolk la croix offal direct trade cliche, palo santo deep v bicycle rights live-edge af yes plz sustainable cornhole hot chicken. Lomo hot chicken church-key big mood venmo.</p>
-                <div className='project__links'>
-                    <a href='/'>Github</a>
-                    <a href='/'>Website</a>
-                </div>
-            </div>
+
         </div>
     )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,17 +7,19 @@ class HomePage extends Component {
         return (
             <div className='home-page'>
                 <div className='home-page__about'>
-                    <h1 className='home-page__about-title'>ENGINEER + DESIGNER</h1>
-                    <p className='home-page__about-paragraph'>
-                        I'm baby craft beer lo-fi single-origin coffee, lomo vexillologist vinyl vaporware hella organic tousled selfies godard raclette. Pour-over echo park pinterest, farm-to-table JOMO poutine mumblecore. Fanny pack bodega boys letterpress butcher art party bespoke drinking vinegar post-ironic readymade bicycle rights vice green juice mustache man bun wolf. Knausgaard fit offal leggings ascot jianbing humblebrag microdosing.
-                    </p>
+                    <h1 className='home-page__about-title'>WEB DEVELOPER</h1>
+                    <div className="home-page__about-split">
+                        <p className='home-page__about-paragraph'>
+                            I'm baby craft beer lo-fi single-origin coffee, lomo vexillologist vinyl vaporware hella organic tousled selfies godard raclette. Pour-over echo park pinterest, farm-to-table JOMO poutine mumblecore. Fanny pack bodega boys letterpress butcher art party.
+                        </p>
+                        <p className="home-page__about-paragraph">
+                        Fanny pack bodega boys letterpress butcher art party bespoke drinking vinegar post-ironic readymade bicycle rights vice green juice mustache man bun wolf. Praxis iceland heirloom tilde fam. Adaptogen flexitarian mlkshk, twee same mukbang try-hard.
+                        </p>
+                    </div>
                 </div>
                 <div className='home-page__projects'>
                     <h2 className='home-page__projects-title'>Projects</h2>
                     <div className='home-page__projects-cards'>
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
                     </div>
                 </div>
                 <div className='home-page__contact'>
